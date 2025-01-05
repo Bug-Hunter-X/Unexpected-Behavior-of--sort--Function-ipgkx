18456019fa04cbab90c4d90761479cfd369f681c
BugHunter-X <askzenai@gmail.com>
2025-01-05 23:24:04 +0100

    Add bugSolution.hs file

---
+++ bugSolution.hs
@@ -0,0 +1,10 @@
+```haskell
+import Data.List (sort)
+
+main :: IO ()
+main = do
+  let xs = [1, 2, 3, 4, 5]
+  let ys = sort xs
+  print (ys == sort xs) -- Expected result: True
+  print (xs == ys) -- Expected result: False
+```
